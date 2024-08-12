fae7ee1d49e39503642070747ec53bf3d5f31efb
jimmydrinkscoffee <jimmyvo.05122002@gmail.com>
2024-08-12 11:59:37 +0700

    boy that thinks his voice matters

--- packages/kafka/src/kafka.module.ts
+++ packages/kafka/src/kafka.module.ts
@@ -1,9 +1,49 @@
-import { Global, Module } from "@nestjs/common"
-import { KafkaService } from "./kafka.service"
+import { DynamicModule, Module, Provider } from "@nestjs/common"
+import {
+  getConnectProvider,
+  getConsumerProvider,
+  getProducerProvider,
+  getSchemaRegistryProvider,
+  IKafkaModuleOptions,
+} from "./kafka.provider"
 
-@Global()
-@Module({
-  providers: [KafkaService],
-  exports: [KafkaService],
-})
-export class KafkaModule {}
+@Module({})
+export class KafkaModule {
+  static register(name: string, opts: IKafkaModuleOptions): DynamicModule {
+    if (!opts.connect) {
+      throw new Error("KafkaModule.register: connect is required")
+    }
+
+    if (!name) {
+      throw new Error("KafkaModule.register: name is required")
+    }
+
+    name = name.toUpperCase()
+
+    type provOpt = {
+      opt: any
+      getProv: (name: string, conf: any) => Provider
+    }
+    const provOpts: provOpt[] = []
+    provOpts.push({ opt: opts.connect, getProv: getConnectProvider })
+    provOpts.push({ opt: opts.producer, getProv: getProducerProvider })
+    provOpts.push({ opt: opts.consumer, getProv: getConsumerProvider })
+    provOpts.push({
+      opt: opts.schemaRegistry,
+      getProv: getSchemaRegistryProvider,
+    })
+
+    const provs: Provider[] = []
+    for (const opts of provOpts) {
+      if (opts.opt) {
+        provs.push(opts.getProv(name, opts.opt))
+      }
+    }
+
+    return {
+      module: KafkaModule,
+      providers: provs,
+      exports: provs,
+    }
+  }
+}
---
+++ packages/kafka/src/kafka.provider.ts
@@ -0,0 +1,76 @@
+import { SchemaRegistry } from "@kafkajs/confluent-schema-registry"
+import { SchemaRegistryAPIClientArgs } from "@kafkajs/confluent-schema-registry/dist/api"
+import { Provider } from "@nestjs/common"
+import {
+  ConsumerConfig,
+  Kafka,
+  KafkaConfig,
+  Producer,
+  ProducerConfig,
+} from "kafkajs"
+
+interface IKafkaConsumerOptions extends ConsumerConfig {
+  topics: string[]
+}
+
+export interface IKafkaModuleOptions {
+  connect: KafkaConfig
+  producer?: ProducerConfig
+  consumer?: IKafkaConsumerOptions
+  schemaRegistry?: SchemaRegistryAPIClientArgs
+}
+
+export function getConnectProvider(name: string, conf: KafkaConfig): Provider {
+  return {
+    provide: `${name}_KAFKA_CONNECT`,
+    useFactory: (): Kafka => {
+      return new Kafka(conf)
+    },
+  }
+}
+
+export function getProducerProvider(
+  name: string,
+  conf: ProducerConfig,
+): Provider {
+  return {
+    provide: `${name}_KAFKA_PRODUCER`,
+    useFactory: async (kafka: Kafka): Promise<Producer> => {
+      const prod = kafka.producer(conf)
+      await prod.connect()
+      return prod
+    },
+    inject: [`${name}_KAFKA_CONNECT`],
+  }
+}
+
+export function getConsumerProvider(
+  name: string,
+  conf: IKafkaConsumerOptions,
+): Provider {
+  return {
+    provide: `${name}_KAFKA_CONSUMER`,
+    useFactory: async (kafka: Kafka): Promise<void> => {
+      const consumer = kafka.consumer(conf)
+      await consumer.connect()
+      await Promise.all(
+        conf.topics.map(async (topic) => {
+          await consumer.subscribe({ topic })
+        }),
+      )
+    },
+    inject: [`${name}_KAFKA_CONNECT`],
+  }
+}
+
+export function getSchemaRegistryProvider(
+  name: string,
+  conf: SchemaRegistryAPIClientArgs,
+): Provider {
+  return {
+    provide: `${name}_SCHEMA_REGISTRY`,
+    useFactory: (): SchemaRegistry => {
+      return new SchemaRegistry(conf)
+    },
+  }
+}
